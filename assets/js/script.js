35c6a5bc4535d185e8c18fdd54d38c741acd7292
reilthebest001 <reilthebest001@gmail.com>
2024-09-05 10:37:23 +0800

    up1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none'; // Hide the loading screen
+  });
+  
 $('.navbar').on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
